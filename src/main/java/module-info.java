4bd25a7b73b80249dbc1e2090e857028ef833965
ekaterina <e.gmerina@g.nsu.ru>
2025-09-16 21:33:46 +0700

    v3 simple ui with green triangle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.parser {
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.feather;
 
     opens org.example.parser to javafx.fxml;
     exports org.example.parser;
