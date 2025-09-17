14f9053f14e703c72a9607d9bba299326c8ed850
ekaterina <e.gmerina@g.nsu.ru>
2025-09-17 16:34:41 +0700

    v6 some menuItems work

--- src/main/java/org/example/parser/UIController.java
+++ src/main/java/org/example/parser/UIController.java
@@ -1,12 +1,23 @@
 package org.example.parser;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class UIController {
     @FXML
@@ -22,12 +33,83 @@ public class UIController {
     private Parent root;
 
     @FXML
+    MenuItem close, save, clear, open;
+
+    @FXML
+    MenuItem language, grammar;
+
+
+    @FXML
     public void initialize() {
 //        fontSizeSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
 //            updateFontSize(newVal.intValue());
 //        });
         setupLineNumbering();
+        setupMenuBar();
 
+    }
+
+    private void setupMenuBar() {
+        close.setOnAction(event -> {
+            Platform.exit();
+        });
+        clear.setOnAction(event -> {
+            contentTextArea.clear();
+        });
+        open.setOnAction(event -> {
+            openFile(event);
+        });
+        save.setOnAction(event -> {
+            saveFile(event);
+        });
+        language.setOnAction(event -> {
+
+        });
+        grammar.setOnAction(event -> {
+
+        });
+    }
+
+    private void saveFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("save file");
+        fileChooser.setInitialFileName("file.txt");
+        Window window = contentTextArea.getScene().getWindow();
+        File file = fileChooser.showSaveDialog(window);
+        if (file != null) {
+            try {
+                Files.writeString(file.toPath(), contentTextArea.getText());
+            } catch (IOException e) {
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Saving error");
+                alert.setHeaderText("can't save the file");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        }
+    }
+
+    private void openFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open txt file");
+
+        Window window = contentTextArea.getScene().getWindow();
+        java.io.File selectedFile = fileChooser.showOpenDialog(window);
+        if (selectedFile != null) {
+            String content = null;
+            try {
+                content = Files.readString(Paths.get(selectedFile.toURI()));
+                contentTextArea.setText(content);
+            } catch (IOException e) {
+                e.printStackTrace();
+                javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("can't open file");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        }
     }
 
 //    private void updateFontSize(int size) {
